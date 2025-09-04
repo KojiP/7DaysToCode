adb3404bd3fe0908cbcf18c688dbfcdb1bb457ef
KojiP <koplu2020@gmail.com>
2025-09-03 21:04:06 -0500

    Dia 1

---
+++ Dia 1/1Day.js
@@ -0,0 +1,32 @@
+let numeroUn = 1;
+
+let stringUn = '1';
+
+let numeroTreinta = 30;
+
+let stringTreinta = '30';
+
+let numeroDiez = 10;
+
+let stringDiez = '10';
+
+// Comparar valor pero no tipo (==)
+if (numeroUn == stringUn) {
+  console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes');
+} else {
+  console.log('Las variables numeroUn y stringUn no tienen el mismo valor');
+}
+
+// Comparar valor y tipo (===)
+if (numeroTreinta === stringTreinta) {
+  console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor y el mismo tipo');
+} else {
+  console.log('Las variables numeroTreinta y stringTreinta no tienen el mismo tipo');
+}
+
+// Comparar valor pero no tipo (==)
+if (numeroDiez == stringDiez) {
+  console.log('Las variables numeroDiez y stringDiez tienen el mismo valor, pero tipos diferentes');
+} else {
+  console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor');
+}
